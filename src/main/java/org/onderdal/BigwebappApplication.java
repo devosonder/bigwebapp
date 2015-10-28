36a162323336fac6c75863d17d7dd9c61a418d47
onder.dal <devos523>
2015-10-28 17:39:18 +0200

    Start.

---
+++ src/main/java/org/onderdal/BigwebappApplication.java
@@ -0,0 +1,12 @@
+package org.onderdal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigwebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigwebappApplication.class, args);
+    }
+}
